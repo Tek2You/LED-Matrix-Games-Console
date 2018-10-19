23332576247b3d41ad97d21061de9060b677db5a
Felix Haschke <haschke.felix@gmail.com>
2018-10-19 14:27:12 +0200

    move tetris into game, make compatible to abstract class game

---
+++ tetris.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "avr.h"
+#include "display.h"
+#include "tetromino.h"
+#include "operators.h"
+#include "game.h"
+
+class Tetris : public Game
+{
+public:
+	Tetris(Display * display);
+	~Tetris();
+	bool up();
+	bool right();
+	bool left();
+	bool down();
+	void reset();
+	void clear();
+	void process();
+	void start();
+	int getPoints(){return points_;}
+
+
+private:
+	void render();
+
+	void checkRowsFinished();
+	bool newTetromino();
+	void takeOverTetromino();
+
+	byte * field_; // field without current tetromino
+	Tetromino * tetromino_;
+	int points_;
+
+	tetromino::SHAPE randomTetrominoShape();
+	tetromino::DIRECTION randomTetrominoDirection(tetromino::SHAPE shape);
+};
