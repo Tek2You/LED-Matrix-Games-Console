ee7c18ab28421909b5b7c3df3a59b31ff8f451fb
Felix Haschke <haschke.felix@gmail.com>
2019-01-17 18:25:13 +0100

    fixup

--- event.cpp
+++ event.cpp
@@ -47,8 +47,7 @@ bool Event::process()
 {
 	processTimers();
 	return (flag(Event::ProcessEveryCycle) || (flag(Event::ProcessPinChanges) && controlButtonChanged()) ||
-	        (flag(Event::ProcessTimerOverflows) && overflow_) ||
-	        (/*flag(Event::ProcessStop) && */ buttonStop().changed()));
+	        (flag(Event::ProcessTimerOverflows) && overflow_) || (flag(Event::ProcessStop) && buttonStop().changed()));
 }
 
 bool Event::processTimers()
