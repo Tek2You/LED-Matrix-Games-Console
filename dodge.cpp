5509b59228732b64b1eaf2b1dfc611537d1bf7d7
Felix Haschke <haschke.felix@gmail.com>
2019-01-22 10:14:09 +0100

    update dodge

--- dodge.cpp
+++ dodge.cpp
@@ -18,6 +18,11 @@
 
 #include "dodge.h"
 
+const Dodge::LineData lines[2] = {
+   {Dodge::LineData::set(3,2,0),{0b11100111,0b11000011, 0b10000001,0}},
+
+};
+
 Dodge::Dodge(Display * display) : Game(display)
 {
 
@@ -25,7 +30,12 @@ Dodge::Dodge(Display * display) : Game(display)
 
 void Dodge::start(Event *event)
 {
-
+	pos_ = Pos(3,15);
+	elements_.removeAll();
+	for(byte i = 0; i < 12; i++){
+		elements_ << 0;
+	}
+	appending();
 }
 
 void Dodge::setSpeed(byte v)
@@ -45,14 +55,12 @@ bool Dodge::onButtonChange(Event *event)
 
 bool Dodge::onTimerOverflow(Event *event)
 {
-
+	if(event->timer(0).overflow()){
+		elements_.removeFirst();
+		appending();
+		render();
+	}
 }
-
-void Dodge::onStop(Event *event)
-{
-
-}
-
 void Dodge::onContinue(Event *event)
 {
 
@@ -60,5 +68,33 @@ void Dodge::onContinue(Event *event)
 
 void Dodge::render()
 {
+	display_->clear();
+	display_->setPixel(pos_);
 
+}
+
+byte Dodge::randomLineId()
+{
+	return millis() % sizeof(lines);
+}
+
+byte Dodge::appendElement(byte element)
+{
+	LineData line_data = lines[element];
+	for(byte i = 0; i < line_data.topMargin(); i++){
+		elements_ << 0;
+	}
+	for(byte i = 0; i < line_data.width(); i++){
+		elements_ << line_data.data_[i];
+	}
+	for(byte i = 0; i < line_data.bottomMargin(); i++){
+		elements_ << 0;
+	}
+}
+
+void Dodge::appending()
+{
+	while(elements_.size() <= 16){
+		appendElement(randomLineId());
+	}
 }
--- dodge.h
+++ dodge.h
@@ -18,27 +18,60 @@
 
 #pragma once
 #include "game.h"
+#include "avr.h"
 
 class Dodge : public Game
 {
 public:
-    Dodge(Display *display);
+	Dodge(Display *display);
 
-    // Game interface
+	// Game interface
 public:
-    void start(Event *event) override;
-    void setSpeed(byte v) override;
-    unsigned int points() const{
-        return points_;
-    }
-    bool isNewHighscore() const;
+	void start(Event *event) override;
+	void setSpeed(byte v) override;
+	unsigned int points() const
+	{
+		return points_;
+	}
+	bool isNewHighscore() const;
 
+	struct LineData
+	{
+		byte settings_;
+		byte data_[4];
+			static byte set(byte width, byte top_margin, byte bottom_margin){
+				if(width > 3) width = 3;
+				if(top_margin > 3) top_margin = 3;
+				if(bottom_margin > 3) bottom_margin = 3;
+				return width | (top_margin << 2) | (bottom_margin << 4);
+			}
+			byte width()
+			{
+				return settings_ & 0b00000011;
+			}
+			byte topMargin()
+			{
+				return (settings_ >> 2) & 0b00000011;
+			}
+			byte bottomMargin()
+			{
+				return (settings_ >> 4) & 0b00000011;
+			}
+	};
 protected:
-    bool onButtonChange(Event *event) override;
-    bool onTimerOverflow(Event *event) override;
-    void onStop(Event *event) override;
-    void onContinue(Event *event) override;
-    void render() override;
+	bool onButtonChange(Event *event) override;
+	bool onTimerOverflow(Event *event) override;
+	void onStop(Event *event) override;
+	void onContinue(Event *event) override;
+	void render() override;
+
 private:
-    unsigned int points_;
+	byte randomLineId();
+	byte appendElement(byte element);
+	void appending();
+
+	unsigned int points_;
+	Pos pos_;
+	List<byte> elements_;
 };
+
