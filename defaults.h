749156abfc19cdc209e2698abb63592e4ce74c1b
Felix Haschke <haschke.felix@gmail.com>
2019-03-08 16:05:05 +0100

    fixed defaults

--- defaults.h
+++ defaults.h
@@ -1,4 +1,4 @@
 #pragma once
 #define DEFAULT_SPEED 2    // 0 - 4
-#define DEFAULT_LANGUAGE 0 // 0 = EN; 1 = DE;
-#define DEFAULT_BRIGHTNESS 0 // 0c d - 3
+#define DEFAULT_LANGUAGE 1 // 0 = EN; 1 = DE;
+#define DEFAULT_BRIGHTNESS 2 // 0c d - 3
