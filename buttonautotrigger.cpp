6c0a99248bb53cc8c655c348989b7587ea768a9f
Felix Haschke <haschke.felix@gmail.com>
2019-05-21 21:52:54 +0200

    add license

--- buttonautotrigger.cpp
+++ buttonautotrigger.cpp
@@ -1,3 +1,20 @@
+/* buttonautotrigger.cpp : for automove with two buttons
+ * Copyright (C) 2019 Felix Haschke
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see http://www.gnu.org/licenses/.
+ */
+
 #include "buttonautotrigger.h"
 
 ButtonAutoTrigger::ButtonAutoTrigger(const Button *button1, const Button *button2, const unsigned int first_interval,
